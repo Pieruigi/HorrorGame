0f43c802da0e1b6926110d75735a66303a927506
Pieruigi <pierluigi.zomparelli@gmail.com>
2025-12-10 18:05:04 +0100

    preparing for itch

--- Assets/SceneAssets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/SceneAssets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -423,6 +423,7 @@ namespace StarterAssets
 				{
 					// the square root of H * -2 * G = how much velocity needed to reach desired height
 					_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+					
 				}
 
 				// jump timeout
